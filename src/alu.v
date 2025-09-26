4e3e19095332f0440d5fd58d9ec1d0f8f2801d29
Angel Scull <scull@Angels-MacBook-Pro.local>
2025-09-26 13:10:45 +0200

    wip

---
+++ src/alu.v
@@ -0,0 +1,20 @@
+module alu16 (
+    input wire [15:0] A,
+    input wire [15:0] B,
+    input wire [7:0] ALUop,
+    output wire [15:0] Result
+);
+    
+endmodule
+
+
+
+module adder16 (
+    input wire  [15:0] A,
+    input wire  [15:0] B,
+    output wire [15:0] Result
+);
+
+ 
+    
+endmodule
